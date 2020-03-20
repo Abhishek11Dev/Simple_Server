019b6e08668cfb4e9f8286b9ed025f95f45a6823
Abhishek11Dev <abhishekkukade9@gmail.com>
2020-03-20 22:09:20 +0530

    nodejs program

---
+++ Simple_Server.js
@@ -0,0 +1,56 @@
+const redis = require("redis");
+const client = redis.createClient();
+//client.set("key",1, redis.print);
+ 
+client.on("error", function(error) {
+  console.error(error);
+});
+                              // redies dependency
+var http = require('http'); // Import Node.js core module
+
+var server = http.createServer(function (req, res) {   //create web server
+    
+     client.incr("count");
+   // client.incr(redisData.val);
+   console.log("-----------------")
+   console.log("count of request in db")
+
+    client.get("count", redis.print);
+    console.log("-----------------")
+    
+	if (req.url == '/') { //check the URL of the current request
+        
+        // set response header
+        res.writeHead(200, { 'Content-Type': 'text/html' }); 
+        
+        // set response content    
+        res.write('<html><body><p>This is home Page.</p></body></html>');
+        res.end();
+    
+    }
+    else if (req.url == "/login") {
+        
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is login Page.</p></body></html>');
+        res.end();
+    
+    }
+    else if (req.url == "/admin") {
+        
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is admin Page.</p></body></html>');
+        res.end();
+    
+   
+    }
+    else
+    {
+          
+           res.end('Invalid Request!');
+    }
+    
+});
+
+server.listen(5000); //6 - listen for any incoming requests
+
+console.log('Node.js web server at port 5000 is running..')
